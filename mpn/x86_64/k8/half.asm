aa17ca22f1d6191562881a046346e0ae54193769
Alex J. Best <alex.j.best@gmail.com>
2016-02-11 12:56:32 +0000

    added timing information for half for k10-2

--- mpn/x86_64/k8/half.asm
+++ mpn/x86_64/k8/half.asm
@@ -21,8 +21,8 @@ dnl  Boston, MA 02110-1301, USA.
 
 include(`../config.m4')
 
-C  | Architecture | Piledriver |
-C  | Cycles/Limb  |    1.4     |
+C  | Architecture | Piledriver |  K10-2  |
+C  | Cycles/Limb  |    1.4     |   1.0   |
 
 ASM_START()
 PROLOGUE(mpn_half)
