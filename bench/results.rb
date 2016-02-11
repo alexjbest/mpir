bac338418b3105f08dcca34611b961586e657f78
Alex J. Best <alex.j.best@gmail.com>
2016-02-11 13:44:47 +0000

    changed results script to show steps of 7 from end

--- bench/results.rb
+++ bench/results.rb
@@ -4,7 +4,7 @@ def ress(name)
   Yaml['functions'].each do |fun|
     if fun['name'] == name
       fun['benchmarks'].each do |b|
-        puts b['results'][991].to_s + " "+  b['results'][995].to_s + " " + b['results'][1000].to_s + "  " + b['hash']
+        puts b['results'][986].to_s + " "+  b['results'][993].to_s + " " + b['results'][1000].to_s + "  " + b['hash']
       end; 0
     end; 0
   end; 0
